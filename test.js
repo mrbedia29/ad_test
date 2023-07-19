1427ae640b3f7a8b5ad22a36f5c3154f0ba7ee63
mrbedia29 <mrbedia@worx.com.ph>
2023-07-19 13:33:16 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// This is a test function
+
+function calculateDaysBetweenDates(begin, end) {
+    let beginDate = new Date(begin);
+    let endDate = new Date(end);
+    let days = (endDate - beginDate)/(1000*60*60*24);
+    return days;
+}
